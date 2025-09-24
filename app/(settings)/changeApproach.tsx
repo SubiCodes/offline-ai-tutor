2075a022c5e6d3d6d16e40f206851d5ac83146da
Rein Subisol <subisolreineerkim@gmail.com>
2025-09-25 05:45:15 +0800

    feat: Installed radio button group from RNR

--- app/(settings)/changeApproach.tsx
+++ app/(settings)/changeApproach.tsx
@@ -1,11 +1,28 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
+import Toast, { Toast as ToastFunc } from 'toastify-react-native'
 
 const ChangeApproach = () => {
   return (
-    <View>
-      <Text>ChangeApproach</Text>
-    </View>
+    <SafeAreaView className='flex-1 justify-start items-start bg-background px-6'>
+      {/* Header */}
+      <View className='w-full flex flex-col gap-2'>
+        <Text className='text-lg text-foreground font-bold'>Change Approach</Text>
+        <Text className='text-md text-gray-500'>Your AI tutor will be taeching you in the manner you've chosen.</Text>
+      </View>
+
+      {/* Seperator */}
+      <View className='min-h-8' />
+
+      {/* Actions */}
+      <View className='w-full flex flex-col gap-4'>
+
+      </View>
+      <Toast />
+    </SafeAreaView>
   )
 }
 
