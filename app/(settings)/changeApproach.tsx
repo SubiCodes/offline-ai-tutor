95e1dd8034d236d232e53fa77b97941b63142d34
Rein Subisol <subisolreineerkim@gmail.com>
2025-09-25 05:55:19 +0800

    feat: Functioning Changeing for tutor's approach

--- app/(settings)/changeApproach.tsx
+++ app/(settings)/changeApproach.tsx
@@ -1,7 +1,8 @@
+import { Label } from '@/components/ui/label';
+import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group';
 import { View, Text } from 'react-native'
 import React, { useCallback, useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
 import Toast, { Toast as ToastFunc } from 'toastify-react-native'
 import { useFocusEffect } from 'expo-router'
@@ -11,6 +12,29 @@ const ChangeApproach = () => {
 
   const [fetchingApproach, setFetchingApproach] = useState<boolean>(true);
   const [approach, setApproach] = useState<string>('');
+  const [savingChanges, setSavingChanges] = useState<boolean>(false);
+
+  const handleSaveChanges = async () => {
+    setSavingChanges(true);
+    await AsyncStorage.setItem('approach', approach);
+    setSavingChanges(false);
+    ToastFunc.show({
+      type: 'success',
+      text1: 'Successfully updated Approach!',
+      text2: 'Your AI tutor will approach you in this manner.',
+      position: 'bottom',
+      visibilityTime: 4000,
+      autoHide: true,
+    })
+  }
+
+  const onLabelPress = (value: string) => () => {
+    setApproach(value);
+  }
+
+  const onValueChange = (value: string) => {
+    setApproach(value);
+  }
 
   const getCurrentApproach = async () => {
     const userNameFromStorage = await AsyncStorage.getItem('approach');
@@ -37,8 +61,25 @@ const ChangeApproach = () => {
 
       {/* Actions */}
       <View className='w-full flex flex-col gap-4'>
-
+        <RadioGroup value={approach} onValueChange={onValueChange}>
+          <View className="flex flex-row items-center gap-3">
+            <RadioGroupItem value="friendly" id="r1" />
+            <Label htmlFor="r1" onPress={onLabelPress('friendly')} className='text-base'>
+              Friendly & Casual
+            </Label>
+          </View>
+          <View className="flex flex-row items-center gap-3">
+            <RadioGroupItem value="professional" id="r2" />
+            <Label htmlFor="r2" onPress={onLabelPress('professional')} className='text-base'>
+              Professional & Direct
+            </Label>
+          </View>
+        </RadioGroup>
+        <Button className='w-full justify-center items-center bg-blue-500' onPress={handleSaveChanges} disabled={savingChanges}>
+          <Text className='text-white font-bold'>Save Changes</Text>
+        </Button>
       </View>
+
       <Toast />
     </SafeAreaView>
   )
