7a4096059276f3cb0034c05c079d9acb93f8ba27
Rein Subisol <subisolreineerkim@gmail.com>
2025-09-24 17:58:54 +0800

    feat: Added drawer navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,7 +3,7 @@ import '@/global.css';
 import { NAV_THEME } from '@/lib/theme';
 import { ThemeProvider } from '@react-navigation/native';
 import { PortalHost } from '@rn-primitives/portal';
-import { Stack } from 'expo-router';
+import Drawer from 'expo-router/drawer';
 import { StatusBar } from 'expo-status-bar';
 import { useColorScheme } from 'nativewind';
 
@@ -18,7 +18,9 @@ export default function RootLayout() {
   return (
     <ThemeProvider value={NAV_THEME[colorScheme ?? 'light']}>
       <StatusBar style={colorScheme === 'dark' ? 'light' : 'dark'} />
-      <Stack />
+        <Drawer>
+          <Drawer.Screen name="index" options={{ headerShown: true }} />
+        </Drawer>
       <PortalHost />
     </ThemeProvider>
   );
