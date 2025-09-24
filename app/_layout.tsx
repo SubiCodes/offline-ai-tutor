639544b41e02de98375a7585a11c4a90a2d1c244
Rein Subisol <subisolreineerkim@gmail.com>
2025-09-25 00:51:04 +0800

    feat: Run the util check on the root layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,26 +1,38 @@
 import '@/global.css';
 
 import { NAV_THEME } from '@/lib/theme';
+import { checkIfKeyExists } from '@/util/checkIfNewUser';
 import { ThemeProvider } from '@react-navigation/native';
 import { PortalHost } from '@rn-primitives/portal';
+import { useNavigationContainerRef } from 'expo-router';
 import Drawer from 'expo-router/drawer';
 import { StatusBar } from 'expo-status-bar';
 import { useColorScheme } from 'nativewind';
+import { useEffect } from 'react';
 
 export {
-  // Catch any errors thrown by the Layout component.
   ErrorBoundary,
 } from 'expo-router';
 
 export default function RootLayout() {
   const { colorScheme } = useColorScheme();
+  const navigationRef = useNavigationContainerRef();
+
+  useEffect(() => {
+    const unsubscribe = navigationRef.addListener('state', async () => {
+      const exists = await checkIfKeyExists('userName');
+      console.log(exists ? 'Key exists ✅' : 'Key does not exist ❌');
+    });
+
+    return unsubscribe;
+  }, [navigationRef]);
 
   return (
     <ThemeProvider value={NAV_THEME[colorScheme ?? 'light']}>
       <StatusBar style={colorScheme === 'dark' ? 'light' : 'dark'} />
-        <Drawer>
-          <Drawer.Screen name="index" options={{ headerShown: true }} />
-        </Drawer>
+      <Drawer>
+        <Drawer.Screen name="index" options={{ headerShown: true }} />
+      </Drawer>
       <PortalHost />
     </ThemeProvider>
   );
