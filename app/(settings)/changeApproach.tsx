e1473e1a3096e804f1101be118c6278ae45050c2
Rein Subisol <subisolreineerkim@gmail.com>
2025-09-25 05:49:33 +0800

    feat: Installed label from RNR

--- app/(settings)/changeApproach.tsx
+++ app/(settings)/changeApproach.tsx
@@ -1,11 +1,29 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useCallback, useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
 import Toast, { Toast as ToastFunc } from 'toastify-react-native'
+import { useFocusEffect } from 'expo-router'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 const ChangeApproach = () => {
+
+  const [fetchingApproach, setFetchingApproach] = useState<boolean>(true);
+  const [approach, setApproach] = useState<string>('');
+
+  const getCurrentApproach = async () => {
+    const userNameFromStorage = await AsyncStorage.getItem('approach');
+    setApproach(userNameFromStorage || '');
+    setFetchingApproach(false);
+  };
+
+  useFocusEffect(
+    useCallback(() => {
+      getCurrentApproach();
+    }, [])
+  );
+
   return (
     <SafeAreaView className='flex-1 justify-start items-start bg-background px-6'>
       {/* Header */}
