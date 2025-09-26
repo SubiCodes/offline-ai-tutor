4c30fe4f8f4d0acf3eaf81d823008b419de746fa
Rein Subisol <subisolreineerkim@gmail.com>
2025-09-26 13:13:40 +0800

    feat: installed axios

--- util/textExtractionFromFiles.ts
+++ util/textExtractionFromFiles.ts
@@ -1,9 +1,9 @@
 import * as FileSystem from "expo-file-system";
 
 type PickedFile = {
-  uri: string;
-  name: string;
-  mimeType?: string; 
+    uri: string;
+    name: string;
+    mimeType?: string;
 };
 
 type ExtractedTextResult = {
@@ -13,37 +13,37 @@ type ExtractedTextResult = {
 }
 
 export async function extractTextFromFile(file: PickedFile): Promise<ExtractedTextResult> {
-  const { uri, mimeType, name } = file;
+    const { uri, mimeType, name } = file;
 
-  try {
-    // ---- TXT ----
-    if (mimeType === "text/plain" || name.endsWith(".txt")) {
-      const text = await FileSystem.readAsStringAsync(uri, {
-        encoding: FileSystem.EncodingType.UTF8,
-      });
-      return {text: text, success: true};
+    try {
+        // ---- TXT ----
+        if (mimeType === "text/plain" || name.endsWith(".txt")) {
+            const text = await FileSystem.readAsStringAsync(uri, {
+                encoding: FileSystem.EncodingType.UTF8,
+            });
+            return { text: text, success: true };
+        }
+
+        // ---- PDF ----
+        if (mimeType === "application/pdf" || name.toLowerCase().endsWith(".pdf")) {
+           
+        }
+
+        // ---- DOC / DOCX ----
+        if (
+            mimeType === "application/msword" ||
+            mimeType ===
+            "application/vnd.openxmlformats-officedocument.wordprocessingml.document" ||
+            name.toLowerCase().endsWith(".doc") ||
+            name.toLowerCase().endsWith(".docx")
+        ) {
+            //   return "[⚠ DOC/DOCX parsing not implemented. Please upload .txt for now]";
+        }
+
+        // ---- Unsupported ----
+        return { success: false, error: "Unsupported file type. Please upload a .txt file." };
+    } catch (error) {
+        console.error("❌ Error extracting text:", error);
+        return { success: false, error: "Unable to parse uploaded file" };;
     }
-
-    // ---- PDF ----
-    if (mimeType === "application/pdf" || name.toLowerCase().endsWith(".pdf")) {
-    //   return "[⚠ PDF parsing not implemented. Please upload .txt for now]";
-    }
-
-    // ---- DOC / DOCX ----
-    if (
-      mimeType === "application/msword" ||
-      mimeType ===
-        "application/vnd.openxmlformats-officedocument.wordprocessingml.document" ||
-      name.toLowerCase().endsWith(".doc") ||
-      name.toLowerCase().endsWith(".docx")
-    ) {
-    //   return "[⚠ DOC/DOCX parsing not implemented. Please upload .txt for now]";
-    }
-
-    // ---- Unsupported ----
-    return {success: false, error: "Unsupported file type. Please upload a .txt file."};
-  } catch (error) {
-    console.error("❌ Error extracting text:", error);
-    return {success: false, error: "Unable to parse uploaded file"};;
-  }
 }
